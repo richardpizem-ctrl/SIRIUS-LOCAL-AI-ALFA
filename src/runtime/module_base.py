5245c722fc82e5605264ed70f916ff0b83b2dcc1
richardpizem-ctrl <richardpizem@gmail.com>
2026-04-22 16:49:07 +0200

    Create module_base.py

---
+++ src/runtime/module_base.py
@@ -0,0 +1,11 @@
+class ModuleBase:
+    def __init__(self, engine):
+        self.engine = engine
+
+    def initialize(self):
+        # TODO: prepare module
+        pass
+
+    def shutdown(self):
+        # TODO: cleanup module
+        pass
