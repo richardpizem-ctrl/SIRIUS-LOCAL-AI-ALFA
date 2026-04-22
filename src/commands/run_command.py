23229d24e03d5fd9bd93308b9e2cfbd6d2c9fbc9
richardpizem-ctrl <richardpizem@gmail.com>
2026-04-23 01:15:10 +0200

    Update run_command.py

--- src/commands/run_command.py
+++ src/commands/run_command.py
@@ -1,1 +1,22 @@
+from .base_command import BaseCommand
+
+
+class RunCommand(BaseCommand):
+    """
+    Príkaz, ktorý spustí akciu alebo úlohu v systéme.
+    """
+
+    name = "run"
+    description = "Spustí zadanú akciu alebo úlohu."
+
+    def execute(self, *args, **kwargs):
+        """
+        Vykoná akciu podľa argumentov.
+        Zatiaľ len demonštračná implementácia.
+        """
+        if not args:
+            return "Nebola zadaná žiadna akcia na spustenie."
+
+        action = args[0]
+        return f"Spúšťam akciu: {action}"
 
