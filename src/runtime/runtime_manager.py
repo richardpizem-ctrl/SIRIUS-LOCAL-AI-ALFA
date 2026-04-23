ba5a08f60d0b0a977c05fb07a7a0c7be0f6773ed
richardpizem-ctrl <richardpizem@gmail.com>
2026-04-23 02:36:01 +0200

    Update runtime_manager.py

--- src/runtime/runtime_manager.py
+++ src/runtime/runtime_manager.py
@@ -1,10 +1,13 @@
 from .engine import RuntimeEngine
 from .event_bus import EventBus
+from triage.aite_controller import AITEController
+
 
 class RuntimeManager:
     def __init__(self):
         self.engine = RuntimeEngine()
         self.events = EventBus()
+        self.aite = AITEController()   # ← AITE integrácia
 
     def initialize(self):
         # TODO: register modules here
